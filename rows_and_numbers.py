89dfb721474d1e789457f1e4d0f87c8a3cf57737
banzai <ban7ai@icloud.com>
2022-07-28 10:03:10 +0300

    edit code

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -4,7 +4,7 @@ print(f'Hi {name_user}. What is your name!')
 
 #2
 number_input = float(input('Enter a fractional number: '))
-print(f'You entered this number: int({number_input})')
+print(f'You entered this number: {int(number_input)}')
 print(f'Integer from user: {int(number_input)}')
 print(f'ТHere is the output of the 4th power of the integer entered by the user: {int(number_input) ** 4}')
 print(f'Outputting the square root of the entered integer: {int(number_input) ** 0.5}')
