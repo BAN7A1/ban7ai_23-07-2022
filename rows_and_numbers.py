4f64eb69075af446df01241757b33bf3c9da517a
banzai <ban7ai@icloud.com>
2022-07-28 10:35:09 +0300

    edit code

--- rows_and_numbers.py
+++ rows_and_numbers.py
@@ -1,3 +1,4 @@
+
 #1
 name_user = input('What is your name? ')
 print(f'Hi {name_user}. What is your name!')
@@ -10,12 +11,4 @@ print(f'ТHere is the output of the 4th power of the integer entered by the user
 print(f'Outputting the square root of the entered integer: {int(number_input) ** 0.5}')
 print(f'Output of the remainder from the division of the entered number: {int(number_input) // 2}')
 
-#3
-def mult_two(a: int, b: int) -> int:
-    first_input = int(input('Enter the first number: '))
-    second_input = int(input('Enter the second number: '))
-    multiplication_of_numbers = first_input * second_input
-    return multiplication_of_numbers
 
-print('Example')
-print(f'The product of two numbers is: {mult_two(1, 2)}')
