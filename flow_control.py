24e34accbb1d9e51403f46749df1a1bc51596e33
BAN7A1 <why-me@tutanota.com>
2022-08-03 11:57:46 +0300

    new

---
+++ flow_control.py
@@ -0,0 +1,42 @@
+while True:
+    try:
+        sign = input('Enter sign (**,*,/,+,-)')
+        if sign not in ('**', '*', '/', '+', '-'):
+            print('You entered an invalid character')
+            break
+
+        if sign in ('**', '*', '/', '+', '-'):
+            first_number = input('Enter the number: ')
+            if first_number.find('.') == 1:
+                first_number = float(first_number)
+            elif first_number.isnumeric() == True:
+                first_number = int(first_number)
+            else:
+                print('You entered an invalid character')
+                break 
+
+        second_number = input('Enter the number: ')
+        if second_number.find('.') == 1:
+            second_number = float(second_number)
+        elif second_number.isnumeric() == True:
+            second_number = int(second_number)
+        else:
+            print('You entered an invalid character')
+            break
+
+        if sign == '**':
+            print(f'Answer = {first_number ** second_number} ')
+        elif sign == '*':
+            print(f'Answer = {first_number * second_number}')
+        elif sign == '/':
+            print(f'Answer = {first_number / second_number}')
+        elif sign == '+':
+            print(f'Answer = {first_number + second_number}')
+        elif sign == '-':
+            print(f'Answer = {first_number - second_number}')
+        else:
+            print('You entered an invalid character')
+            break
+    except (TypeError, ValueError):
+        print('You entered an invalid character')
+    break
