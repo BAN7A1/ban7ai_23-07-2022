6ac24f1f9f1beaaba556992c275f0fdb3b1e4a48
BAN7A1 <why-me@tutanota.com>
2022-08-04 16:28:06 +0300

    edit code

--- flow_control.py
+++ flow_control.py
@@ -6,9 +6,9 @@ while True:
             break
 
         first_number = input('Enter the number: ')
-        if first_number.find('.'):
+        if first_number.find('.') == 1:
             first_number = float(first_number)
-        elif first_number.isnumeric() == True:
+        elif first_number.isnumeric():
             first_number = int(first_number)
         else:
             print('You entered an invalid character')
@@ -17,7 +17,7 @@ while True:
         second_number = input('Enter the number: ')
         if second_number.find('.') == 1:
             second_number = float(second_number)
-        elif second_number.isnumeric() == True:
+        elif second_number.isnumeric():
             second_number = int(second_number)
         else:
             print('You entered an invalid character')
