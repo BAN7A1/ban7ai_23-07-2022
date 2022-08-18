caf09cef9753ebe5b7abb54470ceb012b1fbde44
BAN7A1 <why-me@tutanota.com>
2022-08-18 09:26:30 +0300

    upodate file

--- sets.py
+++ sets.py
@@ -15,3 +15,16 @@ len_number_2 = first_set_2 - first_set_1
 
 print(f'In your entered text such numbers: {len_number_1 - len_number_2}')
 ############################################
+a_set = {1, 52, 224, 12, 9}
+b_set = {'hi', 52, 7, 2, 1}
+c_set = {2, 45, 'hello'}
+
+a_set.update(b_set, c_set)
+print(a_set)
+############################################
+q_set = {1, 56, 2, 14}
+w_set = {43, 4, 1, 12, 2}
+e_set = {5, 15, 2, 9, 56}
+
+q_set |= w_set |= e_set
+print(q_set)
