a19e3607bf7f22abac4c14bb90262537c9f829f3
BAN7A1 <why-me@tutanota.com>
2022-08-21 20:25:16 +0300

    edit function

--- function.py
+++ function.py
@@ -1,9 +1,14 @@
-def input_user(start, end):
+def number_sub(start, end):
 	if start < end:
 		end, start = start, end
+		print('We swapped the values for the correct output of the answer!')
 	return start - end
-
-start_input = int(input())
-end_input = int(input())
-
-print(input_user(start_input, end_input))
+try:
+	start_input = int(input('Enter the first value: '))
+	end_input = int(input('Enter the second value: '))
+	print(f'The response of your operation is: {number_sub(start_input, end_input)}')
+except ValueError:
+	print('You entered the wrong value!')
+##################################################
+def date_time()
+	pass
