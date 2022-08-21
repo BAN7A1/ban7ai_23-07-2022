417bf071342d808b6ac930d662f4dbba0b98ee39
BAN7A1 <why-me@tutanota.com>
2022-08-21 19:32:45 +0300

    edit file

--- dicts.py
+++ dicts.py
@@ -1,9 +1,9 @@
-# new_dict = {element: chr(element) for element in range(6)}
-# print(new_dict)
+new_dict = {element: chr(element) for element in range(6)}
+print(new_dict)
 ##############################
-one_input = input('Enter your message text: ')
+user_input = input('Enter your message text: ')
 password_input = int(input('Enter an integer from 1 to 26: '))
-new_spisk = []
+new_list = []
 eng_dict = {
     'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
     'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
@@ -12,8 +12,8 @@ eng_dict = {
     'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
     'z': 26
 }
-for x in one_input:
+for x in user_input:
     if x in eng_dict:
         new_index = eng_dict[x] + password_input
-        new_spisk.append(list(eng_dict.keys())[list(eng_dict.values()).index(new_index)])
+        new_list.append(list(eng_dict.keys())[list(eng_dict.values()).index(new_index)])
 print(''.join(new_spisk))
