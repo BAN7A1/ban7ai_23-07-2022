ddbc7c385d7aa50952968e5f6077f76fa2af8d9c
BAN7A1 <why-me@tutanota.com>
2022-08-22 18:53:41 +0300

    edit

---
+++ dict.py
@@ -0,0 +1,19 @@
+new_dict = {element: chr(element) for element in range(6)}
+print(new_dict)
+##############################
+user_input = input('Enter your message text: ')
+password_input = int(input('Enter an integer from 1 to 26: '))
+new_list = []
+eng_dict = {
+    'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5,
+    'f': 6, 'g': 7, 'h': 8, 'i': 9, 'j': 10,
+    'k': 11, 'l': 12, 'm': 13, 'n': 14, 'o': 15,
+    'p': 16, 'q': 17, 'r': 18, 's': 19, 't': 20,
+    'u': 21, 'v': 22, 'w': 23, 'x': 24, 'y': 25,
+    'z': 26
+}
+for x in user_input:
+    if x in eng_dict:
+        new_index = eng_dict[x] + password_input
+        new_list.append(list(eng_dict.keys())[list(eng_dict.values()).index(new_index)])
+print(''.join(new_spisk))
